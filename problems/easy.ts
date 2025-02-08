38a78478977fc8ea0ed7b8a62fc77593277acc2c
Erik <erik04.nivala22@gmail.com>
2025-02-07 21:37:16 -0600

    partial finish

--- problems/easy.ts
+++ problems/easy.ts
@@ -10,6 +10,50 @@
  *
  */
 
+const twoSum = (nums: number[], target: number) => {
+  const previousNums = new Map();
+
+  for (let i = 0; i < nums.length; i++) {
+    const remainder = target - nums[i];
+    if (previousNums.has(remainder)) return [previousNums.get(remainder), i];
+
+    previousNums.set(nums[i], i);
+  }
+  console.log(previousNums);
+};
+
+console.log("Running twoSums test cases\n");
+
+// Test Case 1: Basic Example
+console.log("Test Case 1:");
+console.log("Result:  ", twoSum([2, 7, 11, 15], 9));
+console.log("Expected: [0, 1]");
+console.log();
+
+// Test Case 2: Another simple case
+console.log("Test Case 2:");
+console.log("Result:  ", twoSum([3, 2, 4], 6));
+console.log("Expected: [1, 2]");
+console.log();
+
+// Test Case 3: Negative numbers
+console.log("Test Case 3:");
+console.log("Result:  ", twoSum([-3, 4, 3, 90], 0));
+console.log("Expected: [0, 2]");
+console.log();
+
+// Test Case 4: Large numbers
+console.log("Test Case 4:");
+console.log("Result:  ", twoSum([1000000, 500, 999500], 1000000));
+console.log("Expected: [1, 2]");
+console.log();
+
+// Test Case 5: Duplicates in the array
+console.log("Test Case 5:");
+console.log("Result:  ", twoSum([1, 3, 3, 6], 6));
+console.log("Expected: [1, 2]");
+console.log();
+
 /*
  * Problem: Reverse Words in a String
  *
@@ -20,6 +64,14 @@
  * Output: "blue is sky the"
  *
  */
+
+const reverseString = (input: string) => {
+  return input.split(" ").reverse().join(" ");
+};
+
+console.log("this is a test string to reverse");
+console.log(reverseString("this is a test string to reverse"));
+console.log();
 
 /*
  * Problem: Most Common Character
@@ -32,6 +84,26 @@
  *
  */
 
+const mostCommonCharacater = (inputString: string) => {
+  let map = new Map();
+  let highest: { char: string; count: number } = { char: "", count: -Infinity };
+
+  for (const char of inputString) {
+    if (char !== " ") {
+      map.set(char, (map.get(char) || 0) + 1);
+
+      if (map.get(char) > highest.count)
+        highest = { char: char, count: map.get(char) };
+    }
+  }
+
+  return highest.char;
+};
+
+console.log("Most common character:\n");
+console.log("this is a string and should return: s");
+console.log(mostCommonCharacater("this is a string and should return: s"));
+console.log();
 /*
  * Problem: Find Duplicates
  *
@@ -42,6 +114,22 @@
  * Output: [2,3]
  *
  */
+
+const findDuplicates = (inputArray: number[]) => {
+  const found: number[] = [];
+  const result: number[] = [];
+
+  for (const num of inputArray) {
+    if (found.includes(num) && !result.includes(num)) result.push(num);
+    found.push(num);
+  }
+  return result;
+};
+
+const input = [4, 3, 2, 7, 8, 2, 3, 1];
+console.log("Find Duplicates:\n");
+console.log(`input array:${input}`);
+console.log(findDuplicates(input));
 
 /*
  * Problem: First Unique Character
@@ -54,13 +142,18 @@
  *
  */
 
-/*
- * Problem: Find All Duplicates in an Array
- *
- * Given an array, return all duplicate elements.
- *
- * Example:
- * Input: [4,3,2,7,8,2,3,1]
- * Output: [2,3]
- *
- */
+const firstUniqueCharacter = (inputString: string) => {
+  const reversedStringArray = inputString.split("").reverse();
+
+  while (reversedStringArray.length > 0) {
+    const char = reversedStringArray.pop();
+    if (!reversedStringArray.includes(char!)) return char;
+  }
+};
+
+const inputString = "leetcode";
+
+console.log("First Unique Character:\n");
+console.log(`input: ${inputString} | expected to be: "l"`);
+console.log(firstUniqueCharacter(inputString));
+console.log();
--- problems/medium.ts
+++ problems/medium.ts
@@ -9,6 +9,97 @@
  *
  */
 
+const mergeIntervals = (inputArray: number[][]) => {
+  if (inputArray.length <= 1) return inputArray;
+
+  let index = 0;
+  const result: number[][] = [];
+  //first number
+  let currentStart = inputArray[0][0];
+  while (index < inputArray.length - 1) {
+    //current end number is higher then next start number
+    const currentEnd = inputArray[index][1];
+    const nextStart = inputArray[index + 1][0];
+    if (currentEnd < nextStart) {
+      //set start to next index first element
+      result.push([currentStart, inputArray[index][1]]);
+      currentStart = inputArray[index + 1][0];
+    }
+    index++;
+  }
+  result.push([currentStart, inputArray[index][1]]);
+  return result;
+};
+
+console.log(
+  mergeIntervals([
+    [1, 3],
+    [2, 6],
+    [8, 10],
+    [15, 18],
+  ]) + `\nShould be:[[1,6],[8,10],[15,18]]\n\n`
+);
+
+console.log(
+  mergeIntervals([
+    [1, 4],
+    [4, 5],
+  ]) + `\nShould be:[[1,5]]\n\n`
+);
+
+//constraints of the algo???
+// console.log(
+//   mergeIntervals([
+//     [1, 5],
+//     [2, 3],
+//     [4, 6],
+//   ]) + `\nShould be:[[1,6]]\n\n`
+// );
+// console.log(
+//   mergeIntervals([
+//     [1, 10],
+//     [2, 6],
+//     [8, 9],
+//     [11, 15],
+//   ]) + `\nShould be:[[1,10],[11,15]]\n\n`
+// );
+// console.log(
+//   mergeIntervals([
+//     [1, 5],
+//     [2, 7],
+//     [8, 12],
+//     [10, 15],
+//   ]) + `\nShould be:[[1,7],[8,15]]\n\n`
+// );
+// console.log(
+//   mergeIntervals([
+//     [1, 4],
+//     [0, 2],
+//     [3, 5],
+//   ]) + `\nShould be:[[0,5]]\n\n`
+// );
+
+console.log(
+  mergeIntervals([
+    [1, 2],
+    [3, 4],
+    [5, 6],
+  ]) + `\nShould be:[[1,2],[3,4],[5,6]]\n\n`
+);
+
+console.log(mergeIntervals([[1, 3]]) + `\nShould be:[[1,3]]\n\n`);
+
+console.log(mergeIntervals([]) + `\nShould be:[]\n\n`);
+
+console.log(
+  mergeIntervals([
+    [1, 4],
+    [5, 6],
+    [7, 8],
+    [9, 10],
+  ]) + `\nShould be:[[1,4],[5,6],[7,8],[9,10]]\n\n`
+);
+
 /*
  * Problem: Group Anagrams
  *
@@ -19,6 +110,7 @@
  * Output: [["eat","tea","ate"],["tan","nat"],["bat"]]
  *
  */
+const groupAnagrams = (inputArray: string[]) => {};
 
 /*
  * Problem: Longest Palindromic Substring
@@ -30,6 +122,8 @@
  * Output: "bab" (or "aba")
  *
  */
+
+const longestPalindrome = (inputString: string) => {};
 
 /*
  * Problem: Flatten Nested Object
@@ -43,6 +137,11 @@
  * Hint: Use recursion + a helper function.
  */
 
+const flattenObject = (
+  object1: Record<string, any>,
+  object2: Record<string, any>
+) => {};
+
 /*
  * Problem: Deep Object Comparison
  *
@@ -55,6 +154,72 @@
  * Hint: Use recursion and check nested properties.
  */
 
+const compareObjects = (
+  object1: Record<string, any>,
+  object2: Record<string, any>
+) => {
+  if (
+    typeof object1 !== "object" ||
+    object1 === null ||
+    typeof object2 !== "object" ||
+    object2 === null
+  )
+    return false;
+
+  if (Object.entries(object1).length != Object.entries(object2).length)
+    return false;
+
+  for (const [key, value] of Object.entries(object1)) {
+    //check if object2 has same key
+    if (!object2[key]) return false;
+
+    //if another object re call compare on the objects
+    if (typeof value === "object") {
+      if (compareObjects(value, object2[key]) === false) return false;
+    }
+    //check if key values match and if type of values are same
+    else if (object2[key] != value || typeof object2[key] !== typeof value)
+      return false;
+  }
+  return true;
+};
+
+// console.log(
+//   compareObjects({ a: 1, b: 2 }, { a: 1, b: 2 }) + "\nShould be True\n"
+// ); // ✅ true
+// console.log(
+//   compareObjects({ a: { b: 1 } }, { a: { b: 1 } }) + "\nShould be True\n"
+// ); // ✅ true
+// console.log(
+//   compareObjects({ a: 1, b: 2 }, { a: 2, b: 1 }) + "\nShould be False\n"
+// ); // ❌ false
+
+// console.log(
+//   compareObjects({ a: { b: 2, c: 3 } }, { a: { b: 2, c: 3 } }) +
+//     "\nShould be True\n"
+// ); // ✅ true
+// console.log(
+//   compareObjects({ a: { b: 2, c: 4 } }, { a: { b: 2, c: 3 } }) +
+//     "\nShould be False\n"
+// ); // ❌ false
+
+// console.log(
+//   compareObjects({ a: 1, b: "hello" }, { a: 1, b: "hello" }) +
+//     "\nShould be True\n"
+// ); // ✅ true
+// console.log(
+//   compareObjects({ a: 1, b: 2 }, { a: 1, b: "2" }) + "\nShould be False\n"
+// ); // ❌ false
+
+// console.log(compareObjects([1, 2, 3], [1, 2, 3]) + "\nShould be True\n"); // ✅ true
+// console.log(compareObjects([1, 2, 3], [1, 3, 2]) + "\nShould be False\n"); // ❌ false
+// console.log(
+//   compareObjects({ a: [1, 2, 3] }, { a: [1, 2, 3] }) + "\nShould be True\n"
+// ); // ✅ true
+// console.log(
+//   compareObjects({ a: [1, 2, 3] }, { a: [1, 3, 2] }) + "\nShould be False\n"
+// ); // ❌ false
+
 /*
  * Problem: Maximum Subarray Sum
  *
@@ -65,3 +230,35 @@
  * Output: 6  // Subarray: [4,-1,2,1]
  *
  */
+
+const maximumSubarraySum = (inputArray: number[]) => {
+  let result = inputArray[0];
+  let maxEnding = inputArray[0];
+  let resultStartIndex = 0,
+    resultEndIndex = 0;
+  let maxStartIndex = 0,
+    maxEndIndex = 0;
+
+  for (let i = 0; i < inputArray.length; i++) {
+    const newEnding = maxEnding + inputArray[i];
+    maxEndIndex = i;
+    if (newEnding > inputArray[i]) maxEnding = newEnding;
+    else {
+      maxEnding = inputArray[i];
+      maxStartIndex = i;
+    }
+
+    if (maxEnding > result) {
+      resultStartIndex = maxStartIndex;
+      resultEndIndex = maxEndIndex;
+      result = maxEnding;
+    }
+  }
+  return inputArray.slice(resultStartIndex, resultEndIndex + 1);
+};
+
+// const maxSubArrayInput = [-2, 1, -3, 4, -1, 2, 1, -5, 4];
+// console.log("\n\nmaximum sub array:\n");
+// console.log(`input: ${maxSubArrayInput}`);
+// console.log(`result: ${maximumSubarraySum(maxSubArrayInput)}`);
+// console.log("expected: [4,-1,2,1]");
